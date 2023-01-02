d9d5d5bfa91a06d1e9b169e264be715ebf9e2dfa
Fernando <ferobregon03@gmail.com>
2023-01-02 23:09:55 +0100

    Added sphere scene and mouse effect

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,93 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import gsap from 'gsap';
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+
+// Light
+
+const light = new THREE.PointLight(0xffffff, 1.2, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+// Sphere
+
+const geometry = new THREE.SphereGeometry(3, 60, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: 0x00ff00,
+  roughness: 0.6,
+});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+// Cube
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// scene.add(cube);
+
+// Renderer
+const canvas = document.querySelector('.canvas') as HTMLCanvasElement;
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setPixelRatio(2);
+renderer.setSize(sizes.width, sizes.height);
+
+// Resize
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  //   Update scene size
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+function animation() {
+  window.requestAnimationFrame(animation);
+  controls.update();
+  renderer.render(scene, camera);
+}
+animation();
+
+// Sphere color change on mouse move
+let isMouseDown = false;
+window.addEventListener('mousedown', () => (isMouseDown = true));
+window.addEventListener('mouseup', () => (isMouseDown = false));
+window.addEventListener('mousemove', (e) => {
+  if (isMouseDown) {
+    const rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    let { r, g, b } = new THREE.Color(`rgb(${rgb.join(',')})`);
+    gsap.to(sphere.material.color, { r, g, b });
+  }
+});
